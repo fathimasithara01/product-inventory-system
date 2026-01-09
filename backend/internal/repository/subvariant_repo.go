0af05430165e73f7796f1649965a6eb16c7cfcfb
fathima sithara <fathimasithara011@gmail.com>
2026-01-09 13:23:04 +0530

    commit

--- backend/internal/repository/subvariant_repo.go
+++ backend/internal/repository/subvariant_repo.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/fathimasithara01/product-inventory-system/internal/model"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
@@ -26,20 +28,36 @@ func (r *subVariantRepo) Create(ctx context.Context, tx *gorm.DB, subVariants []
 	if len(subVariants) == 0 {
 		return nil
 	}
+
+	for i := range subVariants {
+		if subVariants[i].ID == uuid.Nil {
+			subVariants[i].ID = uuid.New()
+		}
+	}
+
 	return tx.WithContext(ctx).Create(&subVariants).Error
 }
 
 func (r *subVariantRepo) FindByIDForUpdate(ctx context.Context, tx *gorm.DB, id string) (*model.SubVariant, error) {
+	if _, err := uuid.Parse(id); err != nil {
+		return nil, fmt.Errorf("invalid UUID: %s", id)
+	}
+
 	var sub model.SubVariant
 	if err := tx.WithContext(ctx).
 		Clauses(clause.Locking{Strength: "UPDATE"}).
 		First(&sub, "id = ?", id).Error; err != nil {
 		return nil, err
 	}
+
 	return &sub, nil
 }
 
 func (r *subVariantRepo) UpdateStock(ctx context.Context, tx *gorm.DB, subVariant *model.SubVariant) error {
+	if subVariant.ID == uuid.Nil {
+		return fmt.Errorf("invalid UUID: %s", subVariant.ID.String())
+	}
+
 	return tx.WithContext(ctx).
 		Model(&model.SubVariant{}).
 		Where("id = ?", subVariant.ID).
